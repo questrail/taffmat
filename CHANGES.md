db9dcad9f1242c71904c9cd0eb74f1f3c5de5e2b
Matthew Rankin <matthew@questrail.com>
2014-08-06 13:54:10 -0500

    Fix travis.yml deploy section for pypi (hopefully)

--- CHANGES.md
+++ CHANGES.md
@@ -2,7 +2,7 @@
 
 ## develop (unreleased)
 
-## 0.2.1-0.2.5 (August 6, 2014)
+## 0.2.1-0.2.6 (August 6, 2014)
 
 ### Bug Fixes
 
--- taffmat.py
+++ taffmat.py
@@ -59,7 +59,7 @@ from collections import OrderedDict
 # Data analysis related imports
 import numpy as np
 
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 
 def _append_windows_newlines(input_list_of_strings):
